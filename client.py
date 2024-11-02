38dcca758f4b59112a61fea8f80940e72b411be9
James <32513142+jamesryancoleman@users.noreply.github.com>
2024-11-02 17:40:46 -0400

    added TODO to client.py

--- client.py
+++ client.py
@@ -1,5 +1,5 @@
-# TODO implement the grpc client that will call the GetSetRun Server's client
-# the client will be used in client test to call the server.
+# TODO implement the grpc client that will call the GetSetRun Server's Get method
+# the client will be used in client_test.py to call the server.
 
 # see the below reference file
 # https://github.com/jamesryancoleman/bos/blob/main/services/device/drivers/bacnet/client.py
